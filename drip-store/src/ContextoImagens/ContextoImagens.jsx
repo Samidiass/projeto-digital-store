6b67d41885a73d8ce20d12354c561426c4958f22
anacarolinedss <anacaroline22santos@gmail.com>
2024-12-17 19:53:36 -0300

    Adicionando a pasta e arquivo ContextoImagens e algumas funções

---
+++ drip-store/src/ContextoImagens/ContextoImagens.jsx
@@ -0,0 +1,61 @@
+import { createContext, useState } from "react";
+
+    const imagensContexto= {
+    imagens: [slide1, slide2, slide3]
+   // imagensdetalhe: [sabatoAmarelo,, SapatoBege,]
+    cards: [
+
+        {
+        categoria: "Tenis",
+        descricao: "K-Swiss v8 - Masculino",
+        src: tenis, 
+        preco: 200,
+        precoDesconto: 100 
+        }
+       
+        {
+        categoria: "Tenis",
+        descricao: "K-Swiss v8 - Masculino",
+        src: tenis, 
+        preco: 200,
+        precoDesconto: 100 
+        }
+
+        {
+        categoria: "Tenis",
+         descricao: "K-Swiss v8 - Masculino", 
+        src: tenis, 
+        preco: 200,
+        precoDesconto: 100,
+
+        }
+        {
+        categoria: "Tenis",
+        descricao: "K-Swiss v8 - Masculino",
+        src: tenis, 
+        preco: 200,
+        precoDesconto: 100, 
+        }
+
+    ]
+
+}
+
+const ThemeContext= createContext(imagensContexto); 
+
+
+
+const ContextoImagens = (children) => {
+    const = [imgContext, setImgContext] = useState
+    return (
+    <>
+        <ThemeContext.Provider value={{imgContext, setImgContext}}>
+         {children}
+        </ThemeContext.Provider>
+    </>
+
+)
+
+}
+
+export default ContextoImagens
--- drip-store/src/pages/layout.jsx
+++ drip-store/src/pages/layout.jsx
@@ -2,17 +2,22 @@ import Header from "../Compoments/Header/Header"
 import Footer from "../Compoments/Footer/Footer"
 import { BrowserRouter } from "react-router-dom"
 import Rotas from "../Rotas/Rotas.jsx"
+import ContextoImagens from "../ContextoImagens/ContextoImagens.jsx"
 const Layout = () => {
  return (
+<>
 
         <div>
             <BrowserRouter> 
+        <ContextoImagens> 
             <Header />
             <Rotas />
             <Footer />
+        </ContextoImagens>   
             </BrowserRouter>    
         </div>
-    
+      
+</>    
     )
 
 }
